f52d64ff6d38423a16229967054aa56af3e7cdc5
邢汉杰 <xinghanjie@picc.com.cn>
2024-10-03 20:20:35 +0800

    feat: SPM版本

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "JTPromise",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v11)
+        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -26,5 +30,6 @@ let package = Package(
             name: "JTPromiseTests",
             dependencies: ["JTPromise"],
             path: "JTPromiseExample/JTPromiseExampleTests"),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
