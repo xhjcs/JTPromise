7bd5d6c8096ba574f5739a19cdc43e41e379f28e
邢汉杰 <xinghanjie@picc.com.cn>
2025-04-24 15:53:09 +0800

    feat: 优化swift objc混编工程使用

--- Sources/JTPromise/PromiseForObjC.swift
+++ Sources/JTPromise/PromiseForObjC.swift
@@ -1,5 +1,5 @@
 //
-//  __PromiseForObjC__.swift
+//  PromiseForObjC.swift
 //  JTPromise
 //
 //  Created by xinghanjie on 2024/9/29.
@@ -26,12 +26,11 @@ extension PromiseError: CustomNSError {
     }
 }
 
-@objc(JTPromiseSettledResult)
-public class __PromiseSettledResultForObjC__: NSObject {
-    @objc public let result: AnyObject?
+@objc public class JTPromiseSettledResult: NSObject {
+    @objc public let result: Any?
     @objc public let error: Error?
 
-    init(result: AnyObject?) {
+    init(result: Any?) {
         self.result = result
         error = nil
     }
@@ -42,83 +41,82 @@ public class __PromiseSettledResultForObjC__: NSObject {
     }
 }
 
-@objc(JTPromise)
-public final class __PromiseForObjC__: NSObject {
-    private let promise: Promise<AnyObject?>
+@objc public final class JTPromise: NSObject {
+    public let base: Promise<Any?>
 
-    required init(promise: Promise<AnyObject?>) {
-        self.promise = promise
+    public required init(base: Promise<Any?>) {
+        self.base = base
     }
 
-    @objc public static func resolve(_ value: AnyObject?) -> __PromiseForObjC__ {
-        return self.init(promise: Promise(resolve: value))
+    @objc public static func resolve(_ value: Any?) -> JTPromise {
+        return self.init(base: Promise(resolve: value))
     }
 
-    @objc public static func reject(_ error: Error) -> __PromiseForObjC__ {
-        return self.init(promise: Promise(reject: error))
+    @objc public static func reject(_ error: Error) -> JTPromise {
+        return self.init(base: Promise(reject: error))
     }
 
-    @objc public static func promise(executor: @escaping (_ resolve: @escaping (_ value: AnyObject?) -> Void, _ reject: @escaping (_ error: Error) -> Void) -> Void) -> __PromiseForObjC__ {
-        return self.init(promise: Promise(executor: executor))
+    @objc public static func promise(executor: @escaping (_ resolve: @escaping (_ value: Any?) -> Void, _ reject: @escaping (_ error: Error) -> Void) -> Void) -> JTPromise {
+        return self.init(base: Promise(executor: executor))
     }
 
     @discardableResult
-    @objc public func then(_ handler: @escaping (_ value: AnyObject?) -> AnyObject?) -> __PromiseForObjC__ {
-        let swiftPromise = promise.then { value in
+    @objc public func then(_ handler: @escaping (_ value: Any?) -> Any?) -> JTPromise {
+        let swiftPromise = base.then { value in
             let nextValue = handler(value)
-            if let nextPromise = nextValue as? __PromiseForObjC__ {
-                return nextPromise.promise
+            if let nextPromise = nextValue as? JTPromise {
+                return nextPromise.base
             }
             return Promise(resolve: nextValue)
         }
-        return __PromiseForObjC__(promise: swiftPromise)
+        return JTPromise(base: swiftPromise)
     }
 
     @discardableResult
-    @objc public func `catch`(_ handler: @escaping (_ error: Error) -> AnyObject?) -> __PromiseForObjC__ {
-        let swiftPromise = promise.catch { error -> Promise<AnyObject?> in
+    @objc public func `catch`(_ handler: @escaping (_ error: Error) -> Any?) -> JTPromise {
+        let swiftPromise = base.catch { error -> Promise<Any?> in
             let nextValue = handler(error)
-            if let nextPromise = nextValue as? __PromiseForObjC__ {
-                return nextPromise.promise
+            if let nextPromise = nextValue as? JTPromise {
+                return nextPromise.base
             }
             return Promise(resolve: nextValue)
         }
-        return __PromiseForObjC__(promise: swiftPromise)
+        return JTPromise(base: swiftPromise)
     }
 
     @discardableResult
-    @objc public func finally(_ handler: @escaping () -> Void) -> __PromiseForObjC__ {
-        return __PromiseForObjC__(promise: promise.finally(handler))
+    @objc public func finally(_ handler: @escaping () -> Void) -> JTPromise {
+        return JTPromise(base: base.finally(handler))
     }
 
-    @objc public static func all(_ promises: [__PromiseForObjC__]) -> __PromiseForObjC__ {
-        let swiftPromises = promises.map { $0.promise }
-        let newPromise = Promise.all(swiftPromises).then { $0 as AnyObject? }
-        return __PromiseForObjC__(promise: newPromise)
+    @objc public static func all(_ promises: [JTPromise]) -> JTPromise {
+        let swiftPromises = promises.map { $0.base }
+        let newPromise = Promise.all(swiftPromises).then { $0 as Any? }
+        return JTPromise(base: newPromise)
     }
 
-    @objc public static func allSettled(_ promises: [__PromiseForObjC__]) -> __PromiseForObjC__ {
-        let swiftPromises = promises.map { $0.promise }
-        let newPromise = Promise.allSettled(swiftPromises).then { values -> AnyObject? in
+    @objc public static func allSettled(_ promises: [JTPromise]) -> JTPromise {
+        let swiftPromises = promises.map { $0.base }
+        let newPromise = Promise.allSettled(swiftPromises).then { values -> Any? in
             values.map {
                 switch $0 {
                 case let .fulfilled(result):
-                    return __PromiseSettledResultForObjC__(result: result)
+                    return JTPromiseSettledResult(result: result)
                 case let .rejected(error):
-                    return __PromiseSettledResultForObjC__(error: error)
+                    return JTPromiseSettledResult(error: error)
                 }
             } as NSArray
         }
-        return __PromiseForObjC__(promise: newPromise)
+        return JTPromise(base: newPromise)
     }
 
-    @objc public static func any(_ promises: [__PromiseForObjC__]) -> __PromiseForObjC__ {
-        let swiftPromises = promises.map { $0.promise }
-        return __PromiseForObjC__(promise: Promise.any(swiftPromises))
+    @objc public static func any(_ promises: [JTPromise]) -> JTPromise {
+        let swiftPromises = promises.map { $0.base }
+        return JTPromise(base: Promise.any(swiftPromises))
     }
 
-    @objc public static func race(_ promises: [__PromiseForObjC__]) -> __PromiseForObjC__ {
-        let swiftPromises = promises.map { $0.promise }
-        return __PromiseForObjC__(promise: Promise.race(swiftPromises))
+    @objc public static func race(_ promises: [JTPromise]) -> JTPromise {
+        let swiftPromises = promises.map { $0.base }
+        return JTPromise(base: Promise.race(swiftPromises))
     }
 }
